c6041dfc9342664a2b082a20faff3291de2055ab
SuryaRf <ayruzrahmad@gmail.com>
2024-07-22 19:41:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Toogle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+
+// ketika menu diklik
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik di luar side bar untuk menghilangkan side bar
+
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function (e) {
+    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
