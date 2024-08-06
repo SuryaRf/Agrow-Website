64bf1ba0daf0425ccf47bd251fc603a1a4bf715d
SuryaRf <ayruzrahmad@gmail.com>
2024-08-06 23:00:51 +0700

    add products

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ document.querySelector('#menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik di luar side bar untuk menghilangkan side bar
+
+// toogle class active for search form
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+// klik di luar element
+const sb = document.querySelector('#search-button');
 
 const menu = document.querySelector('#menu');
 
 document.addEventListener('click', function (e) {
     if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
 });
